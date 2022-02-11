37447ccd46eafe2aecaa6e01a9986a6b62bb24ea
Jeka <masl.ev@mail.ru>
2022-02-11 21:12:10 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[]args) {
+        Error error = new Error();
+        Error bug = new Error(true, 1, "Найден баг");
+        Error typo = new Error(false, 0, "Опечатка не найдена");
+        Error fail = new Error(true, 1, "Неудача");
+        error.printInfo();
+        bug.printInfo();
+        typo.printInfo();
+        fail.printInfo();
+    }
+}
