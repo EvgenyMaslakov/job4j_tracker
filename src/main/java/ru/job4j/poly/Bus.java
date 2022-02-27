b48b73a4e7442738d53725bdf72d941b05860253
Jeka <masl.ev@mail.ru>
2022-02-27 22:42:35 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        boolean true;
+    }
+
+    @Override
+    public void passengers(int number) {
+    number += number;
+    }
+
+    @Override
+    public double refuel(double numberOfLiters) {
+        double price = 30;
+        return price * numberOfLiters;
+    }
+}
