3cc9dfa1e749911a881e6321805fe43cc14b12b2
Jeka <masl.ev@mail.ru>
2022-03-13 13:37:54 +0300

    1. Singleton [#94606 #274625]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -5,7 +5,16 @@ import java.util.Arrays;
 public final class SingleTracker {
     private Tracker tracker = new Tracker();
 
+    private static SingleTracker instance = null;
+
     private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
     }
 
     public Item add(Item item) {
