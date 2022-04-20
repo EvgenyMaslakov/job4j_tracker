8106faf06ba82cb0cd82f21e889ef6834d04d26f
Jeka <masl.ev@mail.ru>
2022-04-20 20:17:04 +0300

    3. Банковские переводы [#10038 #286708]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,80 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель банковского счета пользователя
+ * @author EVGENY MASLAKOV
+ * @version 1.0
+ */
 public class Account {
+
+    /**
+     * Поле хранит номер банковского счета
+     */
     private String requisite;
+    /**
+     * Поле хранит баланс на банковском счете
+     */
     private double balance;
 
+    /**
+     * Конструктор служит для создания аккаунта пользователя.
+     * Принимает на вход номер банковского счета и баланс на этом счете
+     * @param requisite номер банковского счета
+     * @param balance баланс на этом счете
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Метод служит для получения номера банковского счета пользователя
+     * @return возвращает номер банковского счета
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод служит для изменения номера банковского счета пользователя
+     * @param requisite новый номер банковского счета
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод служит для получения баланса на банковском счете
+     * @return возвращает баланс банковского счета
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод служит для изменения баланса на банковском счете пользователя
+     * @param balance новый баланс банковского счета
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Метод служит для проверки равенства объекта вызвавшего данный метод
+     * и объекта, который поступает в параметрах.
+     * Сначала мы сравниваем ссылки
+     * Если две переменные ссылаются на один блок памяти, то объекты будут однозначно равны.
+     * Проверяем, что аргумент метода не равен null.
+     * Если он равен, то объекты не равны. Это элемент валидации.
+     * Проверяем типы классов.
+     * Объекты могут быть равны, только если классы моделей одинаковые.
+     * Сравниваем поля.
+     * Если поля равны, то два объекта равны тоже.
+     * Для ссылочного типа Object используется метод equals.
+     * @param o объект, который будет сравниваться с текущим объектом
+     * @return возвращает результат сравнения либо true, либо false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +88,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод служит для проверки равенства объектов по хэш-коду
+     * @return возвращает 32-битное число типа int
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,80 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель пользователя банка
+ * @author EVGENY MASLAKOV
+ * @version 1.0
+ */
 public class User {
+
+    /**
+     * Поле хранит номер паспорта пользователя банка
+     */
     private String passport;
+    /**
+     * Поле хранит ФИО пользователя банка
+     */
     private String username;
 
+    /**
+     * Конструктор служит для создания в банковской системе пользователя.
+     * Принимает на вход номер паспорта и ФИО пользователя.
+     * @param passport номер паспорта пользователя
+     * @param username ФИО пользователя
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод служит для получения номера паспорта пользователя.
+     * @return возвращает номера паспорта пользователя.
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод служит для изменения номера паспорта пользователя
+     * @param passport новый номер паспорта пользователя
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод служит для получения ФИО пользователя.
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод служит для изменения ФИО пользователя
+     * @param username новые ФИО пользователя
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Метод служит для проверки равенства объекта вызвавшего данный метод
+     * и объекта, который поступает в параметрах.
+     * Сначала мы сравниваем ссылки
+     * Если две переменные ссылаются на один блок памяти, то объекты будут однозначно равны.
+     * Проверяем, что аргумент метода не равен null.
+     * Если он равен, то объекты не равны. Это элемент валидации.
+     * Проверяем типы классов.
+     * Объекты могут быть равны, только если классы моделей одинаковые.
+     * Сравниваем поля.
+     * Если поля равны, то два объекта равны тоже.
+     * Для ссылочного типа Object используется метод equals.
+     * @param o объект, который будет сравниваться с текущим объектом
+     * @return возвращает результат сравнения либо true, либо false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +88,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Метод служит для проверки равенства объектов по хэш-коду
+     * @return возвращает 32-битное число типа int
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
