46e609cb0f206aa94a2894522a2779ef6cfd9753
Jeka <masl.ev@mail.ru>
2022-05-12 20:45:53 +0300

    1.1. Анонимные классы [#214142]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,25 +1,13 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Comparator;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 0;
-        List<String> rslO1 = new ArrayList<>();
-        for (String el : o1.split("/")) {
-            rslO1.add(el);
-        }
-        List<String> rslO2 = new ArrayList<>();
-        for (String el : o2.split("/")) {
-            rslO2.add(el);
-        }
-        rsl = rslO2.get(0).compareTo(rslO1.get(0));
-        if (rsl == 0) {
-            return o1.compareTo(o2);
-        }
-        return rsl;
+        String[] first = o1.split("/");
+        String[] second = o2.split("/");
+        int rsl = second[0].compareTo(first[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
