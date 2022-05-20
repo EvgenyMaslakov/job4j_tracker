64ed7dd0b166212ca5744e661bb6a9b462183db1
Jeka <masl.ev@mail.ru>
2022-05-20 11:56:39 +0300

    2. Список адресов [#110062 #302691]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,13 +7,15 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
+        return profiles
+                .stream()
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream()
+        return profiles
+                .stream()
                 .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
