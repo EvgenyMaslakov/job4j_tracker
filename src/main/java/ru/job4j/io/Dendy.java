23e402ac73da9a3f4453b00b26acad8e95c79b1e
Евгений <masl.ev@mail.ru>
2023-02-23 23:19:19 +0300

    Dendy

--- src/main/java/ru/job4j/io/Dendy.java
+++ src/main/java/ru/job4j/io/Dendy.java
@@ -8,7 +8,7 @@ public class Dendy {
         boolean run = true;
         while (run) {
             System.out.println("Игры.");
-            System.out.println("1. Танчики");
+            System.out.println("1. ТанчикИ");
             System.out.println("2. Супер Марио");
             System.out.println("3. Выйти");
             System.out.print("Введите пункт меню, чтобы начать игру: ");
