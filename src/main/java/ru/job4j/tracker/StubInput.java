148fcebbb000ca7f8a8532aa2a447d8595050ca8
Евгений <masl.ev@mail.ru>
2023-11-09 08:29:22 +0300

    1.2.5. Полиморфизм 11.0. Шаблон проектирования - Стратегия

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -3,24 +3,46 @@ package ru.job4j.tracker;
 import java.util.List;
 
 /**
- *
+ * Определять работу класса по получению данных из памяти.
+ * Один для консоли, другой для тестов.
  * @author Evgenii Maslakov
  * @version 1.0
  */
 public class StubInput implements Input {
-
+    /**
+     * варианты ответов пользователя
+     */
     private List<String> answers;
+
+    /**
+     * счетчик, чтобы при повторном вызове метода askStr мы получали следующую ячейку из
+     * массива ответов пользователя
+     */
     private int position = 0;
 
+    /**
+     * Принимает варианты ответов пользователя
+     * @param answers варианты ответов пользователя
+     */
     public StubInput(List<String> answers) {
         this.answers = answers;
     }
 
+    /**
+     * возвращает параметры, из заранее подготовленного списка ответов пользователя
+     * @param question сообщение, которое выводится пользователю перед его вводом
+     * @return возвращает параметры, из заранее подготовленного списка ответов пользователя
+     */
     @Override
     public String askStr(String question) {
         return answers.get(position++);
     }
 
+    /**
+     * Возвращать от пользователя число из заранее подготовленного списка
+     * @param question сообщение, которое выводится пользователю перед его вводом
+     * @return Возвращать от пользователя число из заранее подготовленного списка
+     */
     @Override
     public int askInt(String question) {
         return Integer.parseInt(askStr(question));
