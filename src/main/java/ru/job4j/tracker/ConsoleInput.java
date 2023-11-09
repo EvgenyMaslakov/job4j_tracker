e3b8ca41ed161369565b2b1171569a2ce7b2cac9
Евгений <masl.ev@mail.ru>
2023-11-09 07:31:59 +0300

    1.2.5. Полиморфизм 6.1. Разрыв зависимости StartUI от Scanner.

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -3,19 +3,29 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 /**
- *
+ * Определять работу класса по получению данных от пользователя в консоли.
  * @author Evgenii Maslakov
  * @version 1.0
  */
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Возвращает введенную строку от пользователя.
+     * @param question сообщение, которое выводится пользователю перед его вводом
+     * @return Возвращает введенную строку от пользователя.
+     */
     @Override
     public String askStr(String question) {
         System.out.print(question);
         return scanner.nextLine();
     }
 
+    /**
+     * Возвращать от пользователя число, а не строку
+     * @param question сообщение, которое выводится пользователю перед его вводом
+     * @return Возвращать от пользователя число, а не строку
+     */
     @Override
     public int askInt(String question) {
         return Integer.parseInt(askStr(question));
